f320ed84fefd419342f2596ec1147201582070f1
Carlos Beleli <beleli@gmail.com>
2024-03-21 15:13:21 -0300

    bugfix

--- src/main/kotlin/br/com/blifood/api/aspect/LogAndValidate.kt
+++ src/main/kotlin/br/com/blifood/api/aspect/LogAndValidate.kt
@@ -12,6 +12,7 @@ import org.aspectj.lang.reflect.MethodSignature
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -34,7 +35,7 @@ class LogAndValidateAspect {
     @Before("logMethods(logAnnotation)")
     fun logRequest(joinPoint: JoinPoint, logAnnotation: LogAndValidate) {
         val request = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
-        val requestBody = joinPoint.args.firstOrNull()
+        val requestBody = joinPoint.args.lastOrNull()
         if (requestBody is Pageable) {
             getLogger(joinPoint).info("request uri:${request.requestURI}, httpMethod:${request.method}, $requestBody")
         } else if (isRequestBody(joinPoint)) {
@@ -47,8 +48,9 @@ class LogAndValidateAspect {
 
     @AfterReturning(value = "logMethods(logAnnotation)", returning = "returnValue")
     fun logResponse(joinPoint: JoinPoint, logAnnotation: LogAndValidate, returnValue: Any?) {
-        val status = getResponseStatus(joinPoint)
-        getLogger(joinPoint).info("response httpStatus:$status, body:${if (logAnnotation.logResponse) returnValue?.toJsonLog() else "not logged"}")
+        val status = if (returnValue is ResponseEntity<*>) returnValue.statusCode.value() else getResponseStatus(joinPoint)
+        val body = if (returnValue is ResponseEntity<*>) returnValue.body else returnValue
+        getLogger(joinPoint).info("response httpStatus:$status, body:${if (logAnnotation.logResponse) body?.toJsonLog() else "not logged"}")
     }
 
     private fun getLogger(joinPoint: JoinPoint): Logger {
